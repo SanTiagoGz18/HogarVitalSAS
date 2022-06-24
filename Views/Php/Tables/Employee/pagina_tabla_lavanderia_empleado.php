4eab743287bcafddf1a89b759f62366191f9aff3
Andres Sanchez <andresanchez200319@gmail.com>
2022-06-24 11:36:59 -0500

    linkeando todo lo de empleado

--- Views/Php/Tables/Employee/pagina_tabla_lavanderia_empleado.php
+++ Views/Php/Tables/Employee/pagina_tabla_lavanderia_empleado.php
@@ -13,32 +13,40 @@ session_start();
 </head>
 <body>
 	<div class="icono">
-		<a href="../../../../Views/Html/pagina_inicial_producto_empleado.html"><input type="image" id="home" alt="Volver" src="../../../imagenes/home.png"></a><i class="fab fa-Volver"></i>
+		<a href="../../../Html/pagina_inicial_producto_empleado.html"><input type="image" id="home" alt="Volver" src="../../../imagenes/home.png"></a><i class="fab fa-Volver"></i>
 		<span>Volver</span>
 	</div>
 	<h1 class="titulo">LAVANDERIA</h1>
 	<hr width="50%" size="5" color="white" style="top: 14%; position:absolute; left: 25%;">
 	<div class="contenido">
-    	<div id="t1"><p>CODIGO</p></div>
-		<div id="t2"><p>NOMBRE</p></div>
-		<div id="t3"><p>CANTIDAD</p></div>
-		<div id="t4"><p>DETALLES</p></div>
 </body>
 </html>
 <table border="4" class="tabla">
 <tbody>
+	<tr class="tr">
+		<td>CODIGO</td>
+		<td>NOMBRE</td>
+		<td>CANTIDAD</td>
+		<td>FECHA REGISTRO</td>
+		<td>FECHA EDITADO</td>
+		<td>CATEGORIA</td>
+		<td>MARCA</td>
+		<td>MODIFICAR ELIMINAR</td>
+	</tr>
 <?php 
-$select="SELECT * FROM productos WHERE categoria_id='3'";
+$select="SELECT * FROM ((productos INNER JOIN categoria ON productos.categoria_id=categoria.id_categoria)INNER JOIN marca ON productos.marca_id=marca.id_marca)WHERE categoria_id='3'";
 $resultado=mysqli_query($conexion,$select);
 while($fila=mysqli_fetch_array($resultado)){?>
 	<tr>
 		<td><?php echo $fila['id_producto']?> </td>
 		<td><?php echo $fila['nombre_producto']?></td>
 		<td><?php echo $fila['cantidad_producto']?></td>
-		<td><a href="../Views/Html/pagina_productos_detalle_empleados.html"><img src="../../../imagenes/detalles.png" alt="detalles" class="boton"></a></td>
-		</td>
+		<td><?php echo $fila['fecha_registro_producto']?></td>
+		<td><?php echo $fila['fecha_actualizacion_producto']?></td>
+		<td><?php echo $fila['nombre_categoria']?></td>
+		<td><?php echo $fila['nombre_marca']?></td>
+		<td><a href="../Views/Html/pagina_perfil_tabla_empleado.html"><img src="../../../imagenes/detalles.png" alt="detalles" class="boton"></a></td>
 	</tr>
-	
 	</div>
 <?php } ?>
 </tbody>
