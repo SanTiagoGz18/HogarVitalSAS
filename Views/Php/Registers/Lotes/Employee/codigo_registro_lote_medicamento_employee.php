d9974028a701b1dfd48d910475ce7a02eb9fcbde
Mauriciocastano11 <mauriciocastano11@gmail.com>
2022-06-27 16:53:03 -0500

    lote medicamentos empleados links

--- Views/Php/Registers/Lotes/Employee/codigo_registro_lote_medicamento_employee.php
+++ Views/Php/Registers/Lotes/Employee/codigo_registro_lote_medicamento_employee.php
@@ -1,19 +1,19 @@
 <?php
-include("../../../../Models/conexion.php");
+include("../../../../../Models/conexion.php");
 ?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Registrar - Hogar Vital</title>
-	<link rel="stylesheet" type="text/css" href="../../../css/estilo_registro.css">
-	<link rel="icon" type="imagenes/png" href="../../../imagenes/logo_icon.ico">
+	<link rel="stylesheet" type="text/css" href="../../../../css/estilo_registro.css">
+	<link rel="icon" type="imagenes/png" href="../../../../imagenes/logo_icon.ico">
 	<meta charset="utf-8">
 </head>
 <body>
 <div class="contenido2">
-<img src="../../../imagenes/logo.jpeg" class="img">
+<img src="../../../../imagenes/logo.jpeg" class="img">
 <div  class="titulo"><h2>Registrar Lotes Medicamentos</h2></div>
-<form action="../../../../Controller/Create/Admin/codigo_registro_lote_medicamento.php" method="post">
+<form action="../../../../Controller/Create/Employee/codigo_registro_lote_medicamento.php" method="post">
 	<b><label for="marca" id="lmarca">Nombre del lote: </label></b>
 	<br>
 	<input type="text" name="nombre_loteM" placeholder="Ingrese el nombre del lote..." id="imarca" required="">
@@ -23,7 +23,7 @@ include("../../../../Models/conexion.php");
 	<br>
 	<input type="submit" value="Registrar" class="boton" name="enviar">
 </form>
-<center><a href="../../../Php/Tables/Admin/pagina_tabla_lote_medicamento.php"><input type="submit" value="Volver"  name="volver "class="boton2" style="margin-top:-6%"></a></center>
+<center><a href="../../../../Php/Tables/Employee/pagina_tabla_lote_medicamento_empleado.php"><input type="submit" value="Volver"  name="volver "class="boton2" style="margin-top:-6%"></a></center>
 </div>
 </body>
 </html>
