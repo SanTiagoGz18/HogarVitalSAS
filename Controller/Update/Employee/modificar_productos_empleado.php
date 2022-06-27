ab93f4d7938440e6ed86886b1821133669bc9107
SanTiagoGz18 <santiago18203.sg11@gmail.com>
2022-06-27 17:07:48 -0500

    Linkeo pagina de modificar productos empleado

--- Controller/Update/Employee/modificar_productos_empleado.php
+++ Controller/Update/Employee/modificar_productos_empleado.php
@@ -14,20 +14,20 @@ if (isset($_POST['modificado'])){
         $verificar_producto=mysqli_query($conexion, "SELECT * FROM productos WHERE id_producto='$id_producto'");
 
 		if(mysqli_num_rows($verificar_producto)<1){
-			header("location:../../../Views/Html/Verifications/VerificationsProducts/no_existe_modificar_producto.html");
+			header("location:../../../Views/Html/Verifications/VerificationsProducts/Employee/no_existe_modificar_producto_empleado.html");
 			exit();
 		}
 
         if($_REQUEST['categoria'] == 'no'){
-			header("location:../../../Views/Html/Verifications/VerificationsProducts/modificar_agregue_categoria.html");
+			header("location:../../../Views/Html/Verifications/VerificationsProducts/Employee/agregue_categoria_empleado.html");
 			exit();
 		}
         if($_REQUEST['marca'] == 'no'){
-			header("location:../../../Views/Html/Verifications/VerificationsProducts/modificar_agregue_marca.html");
+			header("location:../../../Views/Html/Verifications/VerificationsProducts/Employee/agregue_marca_empleado.html");
 			exit();
 		}
 		if($_REQUEST['proveedor'] == 'no'){
-			header("location:../../../Views/Html/Verifications/VerificationsProducts/modificar_agregue_proveedor.html");
+			header("location:../../../Views/Html/Verifications/VerificationsProducts/Employee/agregue_proveedor_empleado.html");
 			exit();
 		}
 
@@ -36,7 +36,7 @@ if (isset($_POST['modificado'])){
 		$res=mysqli_query($conexion,$update);
 		if($res){
             $update2=mysqli_query($conexion, "UPDATE proveedores_productos SET proveedor_id='".$nombre_proveedor."' WHERE producto_id = '".$id_producto."'");
-			header("location:../../../Views/Html/Verifications/VerificationsProducts/correcto_modificar_productos.html");
+			header("location:../../../Views/Html/Verifications/VerificationsProducts/Employee/correcto_modificar_productos_empleado.html");
 			exit();
 		}
 
